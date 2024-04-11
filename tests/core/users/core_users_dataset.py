c9b17ffdb402d69548f1a0080b117ed982ed5647
GigaWatte <wolfrage2580@gmail.com>
2024-04-12 00:27:29 +0300

    Revert "env setup begin"
    This reverts commit 0fa71773af73fca0c73df45822a0a588beff20e1.

--- tests/core/users/core_users_dataset.py
+++ tests/core/users/core_users_dataset.py
@@ -1,48 +1,14 @@
-from fastapi import status
-
 request_data_set = [
     (
         {
             "username": "Test1",
             "password": "qwerty",
-            "team_id": 0,
+            "team_id": 0,  # Team ID not Exist
             "settings": [],
             "position": "TEST USER",
             "permissions": []
         },
-        {
-            # NotFound: Team id '0' not exist
-            "status": False,
-            "status_code": status.HTTP_404_NOT_FOUND,
-        }
-    ),
-    (
-        {
-            "username": "Test2",
-            "password": "qwerty",
-            "team_id": 1,
-            "settings": [],
-            "position": "TEST USER",
-            "permissions": []
-        },
-        {
-            "status": True,
-            "status_code": status.HTTP_200_OK,
-        }
-    ),
-    (
-        {
-            "username": "Test2",
-            "password": "qwerty",
-            "team_id": 1,
-            "settings": [],
-            "position": "TEST USER",
-            "permissions": []
-        },
-        {
-            # AlreadyExists: User with username 'Test1' already exists
-            "status": False,
-            "status_code": status.HTTP_409_CONFLICT,
-        }
+        True,
+        200
     ),
 ]
