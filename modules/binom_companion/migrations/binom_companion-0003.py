b6e387d944fa980339bb2f16826faf8da99fd4c6
m-riabchenko <mriabchenko11@gmail.com>
2024-05-29 15:29:09 +0300

    fixed migrations

--- modules/binom_companion/migrations/binom_companion-0003.py
+++ modules/binom_companion/migrations/binom_companion-0003.py
@@ -3,8 +3,8 @@ from yoyo import step
 steps = [
     step(
         """
-ALTER TABLE IF EXISTS "binom_companion_proxy_domains" ADD COLUMN is_ready boolean DEFAULT false;
-ALTER TABLE IF EXISTS "binom_companion_proxy_domains" ADD COLUMN server_name character varying(2048);
+ALTER TABLE IF EXISTS "binom_companion_proxy_domains" ADD COLUMN IF NOT EXISTS is_ready boolean DEFAULT false;
+ALTER TABLE IF EXISTS "binom_companion_proxy_domains" ADD COLUMN IF NOT EXISTS server_name character varying(2048);
         """,
         """
 ALTER TABLE IF EXISTS "binom_companion_proxy_domains" DROP COLUMN IF EXISTS is_ready;
